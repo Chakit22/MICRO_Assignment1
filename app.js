ae1ec8120c4c0f42acd06f1b367d8b6413b07056
Chakit <chakitbhandari93@gmail.com>
2022-12-09 21:12:43 +0530

    added error handler middleware

--- app.js
+++ app.js
@@ -9,11 +9,28 @@ const category_model = require("./models/Categories.js"); //Get the category mod
 const image_model = require("./models/galleryImages.js"); //get the images model. Basically gets a collection.
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 app.use("/api/admin",adminRoute);
 
 app.get("/api/health", (req, res) => {
     res.send(`Backend server is active status:active & time:${new Date()}`);
+});
+
+app.use((req,res,next) => {
+    //No route found
+    const err = new Error("Something went wrong! Please try after some time.");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500);
+    res.send({
+        error:{
+            status:err.status || 500,
+            message:err.message
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000,(req,res) => {
--- models/Categories.js
+++ models/Categories.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
-        name:String,
+    name:{
+            type:String,
+            required:true
+        },
     },
     {timestamps:true}
 );
