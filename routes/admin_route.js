9bc64edcc6d3c9bfa1f0b4702b06e81bdd3964fb
Chakit <chakitbhandari93@gmail.com>
2022-12-14 15:47:55 +0530

    added filters to discover api

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -3,27 +3,44 @@ const Image = require("../models/galleryImages.js");
 const {Router} = require("express");
 const route = Router();
 
-route.post("/addCategory",(req,res,next) => {
-    if(!req.body.name)
-        return next(new Error("Name category Required!!"));
-    const newCategory = new Category({
-        name:req.body.name
-    });
-    newCategory.save();
-    res.send("Category Added successfully!!");
+route.post("/addCategory",async (req,res,next) => {
+    try{ 
+        if(!req.body.name){
+            throw new Error("name field is required!!. Please try again!!");
+        }
+        const no_docs = await Category.countDocuments({name:req.body.name});
+        console.log("No of indexes matching this key:value pair are : ",no_docs);
+        if(no_docs != 0){
+            throw new Error("Duplicate value entered!!.Please try again!!");
+        }
+        
+        const newCategory = new Category({
+            name:req.body.name
+        });
+        newCategory.save();
+        res.send("Category Added successfully!!");
+    }catch(err){
+        next(err);
+    }
 });
 
 route.post("/addImage",(req,res,next) => {
-    if(!req.body.name)
-        return next(new Error("Name category Required!!"));
-    const newImage = new Image({
-        name:req.body.name,
-        category:req.body.category,
-        likes:req.body.likes,
-        imageUrl:req.body.imageUrl
-    });
-    newImage.save();
-    res.send("Image Added successfully!!");
+    try{
+        if(!req.body.name){
+            throw new Error("name field is required!!. Please try again!!");
+        }
+        
+        const newImage = new Image({
+            name:req.body.name,
+            category:req.body.category,
+            likes:req.body.likes,
+            imageUrl:req.body.imageUrl
+        });
+        newImage.save();
+        res.send("Image Added successfully!!");
+    }catch(err){
+        next(err);
+    }
 });
 
 module.exports = route;
--- routes/discoverRoute.js
+++ routes/discoverRoute.js
@@ -4,30 +4,40 @@ const Category = require("../models/Categories.js");
 const Image = require("../models/galleryImages.js");
 
 route.get("/getAll",(req,res,next) => {
-    Category.find({},function(err,categories){
-        if(err){
-            next(err);
-            return;
-        }
-        res.send(categories);
-    });
+    try{
+        Category.find({},function(categories){
+            res.send(categories);
+        });
+    }catch(err){
+        next(err);
+    }
 });
 
-route.get("/getImages/:categoryId",(req,res,next) => {
-    const categoryId = req.params.categoryId;
-    const four_images = [];
-    Image.find({},function(err,images){
-        images.forEach((ele) => {
-            if(four_images.length < 4){
-                const image_categories = ele.category;
-                image_categories.forEach((catg) => {
-                    if(categoryId == catg.substr(catg.length - 1))
-                        four_images.push(ele);
-                });
+route.get("/getImages/:category",async (req,res,next) => {
+    try{
+        const sortByDate_order = (!req.query.sortByDate?undefined:(req.query.sortByDate === "asc")?1:-1);
+        const sortByLikes = (req.query.sortByLikes?1:0);
+        const category_name = req.params.category;
+        var docs = "";
+        if(sortByDate_order){
+            if(sortByLikes){
+                docs = await Image.find({category:category_name}).sort({createdAt:sortByDate_order,likes:-1}).limit(4);
+            }else{
+                docs = await Image.find({category:category_name}).sort({createdAt:sortByDate_order}).limit(4);
             }
-        });
-        res.send(four_images);
-    });
+        }else{
+            if(sortByLikes){
+                docs = await Image.find({category:category_name}).sort({likes:-1}).limit(4);
+            }else{
+                docs = await Image.find({category:category_name}).limit(4);
+            }
+        }
+        res.send(docs);
+    }catch(err){
+        next(err);
+    }
 });
+
+
 
 module.exports = route;
